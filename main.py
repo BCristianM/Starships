8fe3cabf34c4d66beec0d19e0ea8c1aba6980662
cmBohoho <cmbohotin@gmail.com>
2022-04-13 12:02:02 +0100

    Just adding the comments

--- main.py
+++ main.py
@@ -25,19 +25,22 @@ def change_names():
     for stuff in list_of_ships:   # This creates a for loop for every key value inside the dictionary
         if stuff["pilots"] is not []:  # This looks at the client lists specifically which are embedded
             for index in range(len(stuff["pilots"])):  # This loops through the lists of the Url themselves, where the constraint is the amount of URL's.
-                pilot_info = requests.get(stuff["pilots"][index]).json()  # Goes into the URL itself and gets the info from
-                pilot_id = db.characters.find_one({"name": pilot_info["name"]}, {"_id": 1})
-                stuff["pilots"][index] = pilot_id["_id"]
+                pilot_info = requests.get(stuff["pilots"][index]).json()  # Goes into the URL itself and gets the info from character
+                pilot_id = db.characters.find_one({"name": pilot_info["name"]}, {"_id": 1})  # This line references the name from the url and /
+                                                                    # looks the _id of the name from the characters list, bringing back the _ID /
+                                                                    # of the character
+                stuff["pilots"][index] = pilot_id["_id"]            # This line replaces the URl with the _id itself, referring to the index of the URL
     return list_of_ships  # This function returns the amended list of ships
 
 
-create_list()
+create_list()      # This calls out the first function
 
 
-sw.delete_many({})
+sw.delete_many({})  # This refreshes the "starships" page everytime this program gets executed, so that there are no
+                    # duplicates
 
 
-sw.insert_many(change_names())
+sw.insert_many(change_names())  # This inserts all the amended key value pairs from the change_names() function
 
 
 
